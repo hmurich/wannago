c3af91049499801ba245a059dca1ab0c0ae5b105
hmurich <hmirich@mail.ru>
2017-05-02 16:37:04 +0600

    fix map

--- public/front/add/map.js
+++ public/front/add/map.js
@@ -1,30 +1,32 @@
-var myMap;
-ymaps.ready(init);
+if (jQuery("#map").length > 0){
+    var myMap;
+    ymaps.ready(init);
 
-function init()
-{
-    lng = jQuery("#map").data('lng');
-    lat = jQuery("#map").data('lat');
-
-    console.log(lng);
-    console.log(lat);
-
-    myMap = new ymaps.Map("map",{
-        center: [lng, lat],
-        zoom: 12,
-        behaviors: ["default", "scrollZoom"]
-    },
+    function init()
     {
-        balloonMaxWidth: 300
-    });
+        lng = jQuery("#map").data('lng');
+        lat = jQuery("#map").data('lat');
+
+        console.log(lng);
+        console.log(lat);
+
+        myMap = new ymaps.Map("map",{
+            center: [lng, lat],
+            zoom: 12,
+            behaviors: ["default", "scrollZoom"]
+        },
+        {
+            balloonMaxWidth: 300
+        });
 
 
 
-    myPlacemark = new ymaps.Placemark([lng, lat]);
-    myMap.geoObjects.add(myPlacemark);
+        myPlacemark = new ymaps.Placemark([lng, lat]);
+        myMap.geoObjects.add(myPlacemark);
 
 
-    myMap.controls.add("zoomControl");
-    myMap.controls.add("mapTools");
-    myMap.controls.add("typeSelector");
+        myMap.controls.add("zoomControl");
+        myMap.controls.add("mapTools");
+        myMap.controls.add("typeSelector");
+    }
 }
