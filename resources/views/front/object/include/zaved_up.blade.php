04f85902f2eafbf331998d9a165cca03fff3510a
hmurich <hmirich@mail.ru>
2017-05-04 14:14:35 +0600

    fix

--- resources/views/front/object/include/zaved_up.blade.php
+++ resources/views/front/object/include/zaved_up.blade.php
@@ -18,7 +18,7 @@
         <div class="zaved-book">
             <a class="zaved-book__button btn fancybox fancybox.ajax" href="{{ action('Front\Object\ReserveController@getForm', $object->id) }}">Забронировать столик</a>
             @if ($object->relLocation && $object->relLocation->lng && $object->relLocation->lat)
-                <a class="zaved-book__map" href="#map">Посмотреть на карте</a>
+                <a class="zaved-book__map" href="{{ action('Front\Object\ShowController@getIndex', $object->alias) }}#map">Посмотреть на карте</a>
             @endif
         </div>
     </div>
